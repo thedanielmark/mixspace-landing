22776bc63d756597b6849795bd7d1aa40e2976c4
thedanielmark <danielmark.uc@gmail.com>
2020-12-13 16:48:44 +0530

    modified:   mail-config.php

--- mail-config.php
+++ mail-config.php
@@ -1,4 +1,7 @@
 <?php
+
+include("../passwords.php");
+
 $mail->From = "noreply.danielmarkd@gmail.com";
 $mail->FromName = "MixSpace Internet Services";
 $mail->Host = "smtp.gmail.com"; // specif smtp server
@@ -6,4 +9,4 @@ $mail->SMTPSecure = "ssl"; // Used instead of TLS when only POP mail is selected
 $mail->Port = 465; // Used instead of 587 when only POP mail is selected
 $mail->SMTPAuth = true;
 $mail->Username = "noreply.danielmarkd@gmail.com"; // SMTP username
-$mail->Password = "icanttellyou1029384756"; // SMTP password
+$mail->Password = $mxOutGmailPassword; // SMTP password
